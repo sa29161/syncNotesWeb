4a56398acc61c6b39dad5aebeb1bb4c6ad20cc11
sa29161 <sa1489053@gmail.com>
2021-01-25 17:57:22 -0500

    updated login page

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import fire from './config/fire.js';
+import { Form, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import LoginCSS from './config/LoginCSS.css';
 
 class Login extends React.Component{
 
@@ -27,20 +29,30 @@ class Login extends React.Component{
           })
       }
 
-    render(){
-        return(
-            <div style={{ textAlign: 'center' }}>
-            <div>
-              <div>Email</div>
-              <input id="email" placeholder="Enter Email.." type="text"/>
+    render() {
+        return (
+            <div className="user-form">
+            <div className="login-form" style={{ textAlign: 'center' }}>
+                <img src="https://www.freelogodesign.org/file/app/client/thumb/78c5dee8-04dd-44a1-a851-8abe807920ab_200x200.png?1611543788336"/>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control className="input" id="email" style = {{width: '20%', left: '40%', position: 'relative'}} type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control id="password" style = {{width: '20%', left: '40%', position: 'relative'}} type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                    </Form.Group>
+                </Form>
+                <Button variant="dark" style={{ margin: '10px' }} onClick={this.login}>Login</Button>
+                <Button variant="secondary" style={{ margin: '10px' }} onClick={this.signUp}>Sign Up</Button>
             </div>
-            <div>
-              <div>Password</div>
-              <input id="password" placeholder="Enter Password.." type="text"/>
             </div>
-            <button style={{margin: '10px'}} onClick={this.login}>Login</button>
-            <button style={{margin: '10px'}} onClick={this.signUp}>Sign Up</button>
-          </div>
+   
+     
         )
 
 
